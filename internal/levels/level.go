7a95fd64b047fac52c31ac6694bbfacd92c204e7
Juan Antonio Cid Tomas <juanantonio.cid@cabify.com>
2025-03-14 15:45:30 +0100

    Refactor levels to a data-driven design

--- internal/levels/level.go
+++ internal/levels/level.go
@@ -5,52 +5,14 @@ import "github.com/juanancid/maze-adventure/internal/ecs"
 func CreateLevelWorld(level int) *ecs.World {
 	world := ecs.NewWorld()
 
-	switch level {
-	case 0:
-		createLevelWorld0(world)
-	case 1:
-		createLevelWorld1(world)
-	default:
-		createLevelWorldDefault(world)
+	config, ok := levelConfigs[level]
+	if !ok {
+		config = levelConfigs[-1] // use default config if not found.
 	}
+
+	createPlayer(world, config.playerSize, config.cellSize)
+	createMaze(world, config.mazeWidth, config.mazeHeight, config.cellSize)
+	createExit(world, config.mazeWidth-1, config.mazeHeight-1, config.cellSize)
+
 	return world
-}
-
-func createLevelWorld0(world *ecs.World) {
-	const (
-		mazeWidth  = 8
-		mazeHeight = 5
-		cellSize   = 40
-		playerSize = 12
-	)
-
-	createPlayer(world, playerSize, cellSize)
-	createMaze(world, mazeWidth, mazeHeight, cellSize)
-	createExit(world, mazeWidth-1, mazeHeight-1, cellSize)
-}
-
-func createLevelWorld1(world *ecs.World) {
-	const (
-		mazeWidth  = 16
-		mazeHeight = 10
-		cellSize   = 20
-		playerSize = 12
-	)
-
-	createPlayer(world, playerSize, cellSize)
-	createMaze(world, mazeWidth, mazeHeight, cellSize)
-	createExit(world, mazeWidth-1, mazeHeight-1, cellSize)
-}
-
-func createLevelWorldDefault(world *ecs.World) {
-	const (
-		mazeWidth  = 32
-		mazeHeight = 20
-		cellSize   = 10
-		playerSize = 6
-	)
-
-	createPlayer(world, playerSize, cellSize)
-	createMaze(world, mazeWidth, mazeHeight, cellSize)
-	createExit(world, mazeWidth-1, mazeHeight-1, cellSize)
 }
